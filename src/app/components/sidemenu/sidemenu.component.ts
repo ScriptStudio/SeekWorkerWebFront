01426c1fe89219e8985e8ef280a562c99b3431ce
Matheus <matheus.saraiva@gmail.com>
2020-08-28 17:17:16 -0300

    Habilitado a responsividade do sidemenu e adicionado parâmetros de compilação do tsconfig. Fixes #8

--- src/app/components/sidemenu/sidemenu.component.ts
+++ src/app/components/sidemenu/sidemenu.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { SidemenuService } from './sidemenu.service';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 
 @Component({
@@ -10,13 +11,23 @@ import { SidemenuService } from './sidemenu.service';
 })
 export class SidemenuComponent implements AfterViewInit {
 
+  public matcher: MediaQueryList;
+
   @ViewChild('drawer') public sideMenu: MatDrawer;
   
-  constructor(private sideMenuService: SidemenuService) { }
+  constructor(
+    private sideMenuService: SidemenuService, 
+    private mediaMatcher: MediaMatcher
+  ) 
+  {
+    this.matcher = this.mediaMatcher.matchMedia('(max-width: 600px)');
+  }
 
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
+  public ngAfterViewInit(): void {
     this.sideMenuService.sideMenu = this.sideMenu;
+  }
+
+  public sideMenuMode(): string {
+    return this.matcher.matches ? 'over' : 'side';
   }
 }
